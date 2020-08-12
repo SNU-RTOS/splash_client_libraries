8aa45bb2ca5e7a4e04bd39faa972662aa99036c9
khc196 <khc146@gmail.com>
2020-08-12 17:58:29 +0900

    Add Factory

--- scl/factory.py
+++ scl/factory.py
@@ -2,7 +2,16 @@ from .impl.singleton import Singleton
 
 
 class Factory(Singleton):
-    def __init__(self, name, parent, mode_configuration):
+    def __init__(self, name, parent, mode_configuration, mode):
         self.name = name
         self.parent = parent
         self.mode_configuration = mode_configuration
+        self.mode = mode
+
+    def get_namespace(self):
+        parent = self.parent
+        name = self.name
+        name = self.mode + "/" + name if self.mode else name
+        if(parent):
+            name = parent.get_namespace() + "/" + name
+        return name
